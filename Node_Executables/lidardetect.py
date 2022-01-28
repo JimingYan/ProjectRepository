190b9cfba774cc66503cbc7dddec4eeb42e7ba12
JimingYan <84682886+JimingYan@users.noreply.github.com>
2022-01-28 01:57:39 -0500

    Update lidardetect.py

--- Node_Executables/lidardetect.py
+++ Node_Executables/lidardetect.py
@@ -14,27 +14,27 @@ def callback(data):
     global pub1
     global pub2
     global pub3
-    front_left =(data.ranges[0:10])
+    front_left =(data.ranges[0:10])  ## create cone of detection infront of lidar 
     front_right=(data.ranges[-10:])
     front = front_right+front_left
     Min=min(front)
-    if(Min>1.5):
+    if(Min>=0.5):         ## if no object for 0.5m in front of the car, run speed command to VESC for forward motion. 
         verd="far"
-        pub1.publish(5000)
+        pub1.publish(5000)      
         pub3.publish(0.48)
-    if(Min<1.5):
+    if(Min<0.5):         ## if obstacle is detected, start brakes 
         verd="near"
         brake()
     print verd
-def brake():
+def brake():              ## create a subsriber that listens to /sensors/core
     rospy.Subscriber("/sensors/core",VescStateStamped ,applybrake)
     rospy.spin() 
 
 def applybrake(data):
     global pub2
     global pub1
-    if(data.state.speed>500):
-        pub2.publish(-0.006*data.state.speed)
+    if(data.state.speed>500): ## execute a braking command to stop the motor
+        pub2.publish(15)
     else:
         pub2.publish(0.0)
 def lidardetect():
@@ -44,7 +44,7 @@ def lidardetect():
     rospy.init_node("LidarDetect", anonymous=True)
     rospy.Subscriber("/scan", LaserScan, callback)
     pub1 = rospy.Publisher("commands/motor/speed", Float64, queue_size=10)
-    pub2 = rospy.Publisher("commands/motor/current", Float64, queue_size=10)
+    pub2 = rospy.Publisher("commands/motor/brake", Float64, queue_size=10)
     pub3 = rospy.Publisher("commands/servo/position",Float64, queue_size=10)
     rospy.spin()
 
